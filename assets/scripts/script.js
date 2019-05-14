18055777487c3199a865f69c39a0d843a54fc5a4
fkakatie <sextonskatie@gmail.com>
2019-05-13 21:03:31 -0600

    current year in footer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('JS file loaded!');
+    
+    // put current year in footer
+    document.getElementById('copy-year').innerHTML = new Date().getFullYear();
 });
